bbd64573c7b0d5314a51fe90280e6ae588fe0712
shravan kumar <slmalviya32@gmail.com>
2024-06-24 20:58:51 +0530

    first commit

---
+++ Pr-1_SquareRoot.js
@@ -0,0 +1,18 @@
+function squreRoot(number) {
+    if ( number < 0 ){
+        return NaN;
+    }
+    let guess = number / 2 ;
+    let preGuess;
+
+    while (guess  !== preGuess) {
+        preGuess = guess;
+        guess = (guess + number / guess) / 2
+    }
+    return guess;
+}
+
+const number = 16;
+const result = squreRoot(number);
+console.log(result);
+
