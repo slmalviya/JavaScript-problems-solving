848bb95bb52e389475febce7ca5c4ccc9e2d5247
shravan kumar <slmalviya32@gmail.com>
2024-06-26 13:16:04 +0530

    commit

---
+++ Pr-23__ObjInArray.js
@@ -0,0 +1,7 @@
+
+let myArray = [1, 2, 3];
+let myObject = { name: "Alice", age: 25 };
+
+
+console.log(Array.isArray(myArray));  // Output: true
+console.log(Array.isArray(myObject)); // Output: false
