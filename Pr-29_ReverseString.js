99942711cb8e180050007bf02cd999b7dbad29ce
shravan kumar <slmalviya32@gmail.com>
2024-06-26 16:56:54 +0530

    commit

---
+++ Pr-29_ReverseString.js
@@ -0,0 +1,10 @@
+function reverseString(str) {
+    return str.split('').reverse().join('');
+  }
+  
+
+  const originalString = "hello";
+  const reversedString = reverseString(originalString);
+  
+  console.log(reversedString); // Output: "olleh"
+  
